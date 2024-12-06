c7e52ba9a5b3f0b21aac84bba4bfed193730f055
maheshsk61 <maheshsk61@gmail.com>
2024-12-06 08:24:42 +0530

    implemented grid2 for product lists page

--- client/src/components/product-list/product-list.tsx
+++ client/src/components/product-list/product-list.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { productLists } from "../../api";
-import { Box, Grid2, Card, Avatar, Typography, Skeleton } from "@mui/material";
+import { Box, Card, Typography, Skeleton, Grid2 } from "@mui/material";
 import "./product-list.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../../Redux/store";
@@ -47,16 +47,28 @@ const ProductList: React.FC = (): JSX.Element => {
         {loading ? (
           Array.from({ length: lists.length || 21 }, (_, index) => {
             return (
-              <Grid2 size={{ xs: 12, sm: 6, md: 4 }} key={index}>
+              <Grid2
+                sx={{
+                  gridColumn: {
+                    xs: "span 12", // Full width on extra-small screens
+                    sm: "span 6", // Half width on small screens
+                    md: "span 4", // One-third width on medium screens
+                    lg: "span 4", // Same as md
+                    xl: "span 3", // One-fourth width on extra-large screens
+                  },
+                }}
+                key={index}
+              >
                 <Card
                   sx={{
+                    width: "250px",
                     padding: 2,
-                    borderRadius: 20,
+                    borderRadius: 5,
                     background: "var(--silver-color)",
                   }}
                 >
                   <Box className="d-flex justify-content-center ">
-                    <Skeleton variant="circular" width={200} height={200} />
+                    <Skeleton variant="rectangular" width={200} height={200} />
                   </Box>
                 </Card>
               </Grid2>
@@ -65,19 +77,30 @@ const ProductList: React.FC = (): JSX.Element => {
         ) : filteredProducts.length > 0 ? (
           filteredProducts.map((product: IProduct) => {
             return (
-              <Grid2 size={{ xs: 12, sm: 6, md: 4 }} key={product.id}>
+              <Grid2
+                sx={{
+                  gridColumn: {
+                    xs: "span 12", // Full width on extra-small screens
+                    sm: "span 6", // Half width on small screens
+                    md: "span 4", // One-third width on medium screens
+                    lg: "span 4", // Same as md
+                    xl: "span 3", // One-fourth width on extra-large screens
+                  },
+                }}
+                key={product.id}
+              >
                 <Card
-                  sx={{ width: "fitContent", padding: 2, borderRadius: 20 }}
+                  sx={{ width: "250px", padding: 2, borderRadius: 5 }}
                   className="card"
                 >
-                  <Avatar
+                  <img
                     src={`http://localhost:4000${product.imageUrl}`}
                     alt={product.name}
-                    sx={{
+                    style={{
                       width: 200,
                       height: 200,
-                      objectFit: "cover",
                       margin: "0 auto",
+                      objectFit: "cover",
                     }}
                   />
                   <Typography variant="h6" sx={{ textAlign: "center" }}>
